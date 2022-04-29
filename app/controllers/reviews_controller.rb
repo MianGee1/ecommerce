4422e6a456ea49a15a5462347a6efa6e5af1c96f
MianGee1 <umersajjad101@gmail.com>
2022-04-29 17:38:30 +0500

    Delete Product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,27 +1,3 @@
 class ReviewsController < ApplicationController
-  def index
-    @review = Review.all
-  end
 
-  def show
-  end
-
-  def new
-    @review = Product.reviews.build
-  end
-
-  def edit
-  end
-
-  def create
-    @product = Product.find(params[:product_id])
-    @review = @product.reviews.create(review_params)
-    redirect_to product_path(@product)
-  end
-
-  private
-
-    def review_params
-      params.require(:review).permit(:content)
-    end
 end
