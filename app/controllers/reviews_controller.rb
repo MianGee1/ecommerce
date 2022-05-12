900d8c0f137bb8148156fd4ff7d3bb1c83c82f70
MianGee1 <umersajjad101@gmail.com>
2022-05-12 11:53:07 +0500

    merge method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,36 +7,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    # @review = @product.reviews.new.tap do |r|
-    #   @r.review_id = review_params.review_id
-    #   r.content = review_params.content
-    #   r.user = current_user
-    # end
     @review = @product.reviews.new(review_params.merge(user: current_user))
 
     respond_to do |format|
       if @review.save
-        format.js
         format.html { redirect_to product_path(@product), notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
-        format.js
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @review.errors, status: :unprocessable_entity }
+
       end
     end
   end
-
-
-  # user = User.new.tap do |u|
-  #   u.username = "kartik"
-  #   u.save!
-  # end
-
-  # # Old Dusty Traditional Method
-  # user = User.new
-  # user.username = "kartik"
-  # user.save!
 
   def update
     @review = @product.reviews.find(params[:id])
